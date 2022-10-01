493084c4561de9538dfe03d647986599199f56da
tahos81 <tahirozpala@gmail.com>
2022-10-02 02:10:39 +0300

    Update RewardNFT.sol

--- contracts/RewardNFT.sol
+++ contracts/RewardNFT.sol
@@ -3,6 +3,25 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+interface IERC20 {
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
 contract RewardNFT is ERC721 {
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    IERC20 paymentToken;
+    uint256 private _totalSupply;
+    uint256 public mintPrice;
+
+    constructor(string memory name, string memory symbol, address paymentTokenAddress, uint256 _mintPrice) ERC721(name, symbol) {
+        paymentToken = IERC20(paymentTokenAddress);
+        mintPrice = _mintPrice;
+    }
+
+    function mint() external {
+        bool success = paymentToken.transferFrom(msg.sender, address(this), mintPrice);
+        require(success);
+        _mint(msg.sender, _totalSupply);
+        unchecked { _totalSupply++; }
+    }
 }
