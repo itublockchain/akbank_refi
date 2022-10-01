4b5ab303ebea2ea8a7a1a0eddd033b814592b10f
tahos81 <tahirozpala@gmail.com>
2022-10-01 19:49:49 +0300

    membership nft

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Fundraiser is ERC1155 {
+    uint256 constant HUNDRED_PERCENT_BPS = 10_000;
 
     constructor(string memory uri) ERC1155(uri) {
 
